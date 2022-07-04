f5643296492063895958c2c77b329db02b4a81a0
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2022-07-04 03:26:05 -0400

    Integra Spring Boot

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,13 @@ module com.interview.oriontekchallenge {
     requires javafx.fxml;
     requires java.sql;
     requires de.jensd.fx.glyphs.fontawesome;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.beans;
 
 
-    opens com.interview.oriontekchallenge to javafx.fxml;
+    opens com.interview.oriontekchallenge to javafx.fxml, spring.core;
     exports com.interview.oriontekchallenge;
     exports com.interview.oriontekchallenge.controller;
     opens com.interview.oriontekchallenge.controller to javafx.fxml;
